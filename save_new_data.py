9321e3ef8e40252b94887c171573216db7ca66b3
Andercou <sandu.andercou@gmail.com>
2024-04-10 10:21:54 +0300

    first

--- save_new_data.py
+++ save_new_data.py
@@ -1,8 +1,9 @@
 
 import os
 import re
-import subprocess
 import shutil
+import time
+
 pathC="C:/Users/Alexandru Andercou/Downloads/"
 
 baseName="infinitysave"
@@ -11,23 +12,21 @@ ext=".json";
 
 regexp = re.compile(pattern)
 
-files=os.listdir(path=pathC)
+
+while 1:
 
 
+ files=os.listdir(path=pathC)
 
-for file in files:
+ for file in files:
   if re.search(pattern,file) is not None:
     nameFile=pathC+file
     shutil.copy(nameFile, './')
     print(file)
 
-os.system("git add .")
-
-
-
-os.system("git commit -m \"first\"")
-
-os.system("git push origin main")
-
+ os.system("git add .")
+ os.system("git commit -m \"first\"")
+ os.system("git push origin main")
+ time.sleep(120000)
 
 
