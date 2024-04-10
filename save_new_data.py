3b3d2628214e3475536faf146294c50d598c9267
Andercou <sandu.andercou@gmail.com>
2024-04-10 10:17:52 +0300

    first

---
+++ save_new_data.py
@@ -0,0 +1,34 @@
+
+import os
+import re
+import subprocess
+import shutil
+pathC="C:/Users/Alexandru Andercou/Downloads/"
+
+baseName="infinitysave"
+pattern=r''+baseName+'*'
+ext=".json";
+
+regexp = re.compile(pattern)
+
+files=os.listdir(path=pathC)
+
+
+
+for file in files:
+  if re.search(pattern,file) is not None:
+    nameFile=pathC+file
+    shutil.copy(nameFile, './')
+    print(file)
+
+os.system("git add .")
+
+
+
+os.system("git commit -m \"first\"")
+
+
+
+
+
+
