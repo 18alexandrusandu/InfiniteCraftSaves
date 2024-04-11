6444c9f78db90d17ae87f6287a2c31a131ca39b1
Andercou <sandu.andercou@gmail.com>
2024-04-11 10:00:21 +0300

    first

--- save_new_data.py
+++ save_new_data.py
@@ -3,7 +3,7 @@ import os
 import re
 import shutil
 import time
-
+import datetime
 pathC="C:/Users/Alexandru Andercou/Downloads/"
 
 baseName="infinitysave"
@@ -21,10 +21,19 @@ while 1:
  print("again")
  files=os.listdir(path=pathC)
  filesHere=os.listdir(path=pathC)
+
+ 
+ now = time.time()
  
  status = os.stat(pathC).st_mtime
- print("status:")
+ print("status download folder:")
  print(status)
+ print("now:")
+ print(now)
+ 
+ 
+ 
+ 
  for file in files:
   if (re.search(pattern,file) is not None) or  (re.search(pattern2,file) is not  None):
     nameFile=pathC+file
